8fccea4d73c95ddefdd7cd4c70581ab35740db12
Kike <kike-0203@users.noreply.github.com>
2025-10-29 16:08:43 -0600

    Render: .gitignore + requirements (fix regex) + app.py + render.yaml

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
-﻿from flask import Flask
-
-app = Flask(__name__)
-
-@app.get("/")
-def index():
-    return "Hola desde Render - Flask mínimo"
+﻿from app import app as app
 
 if __name__ == "__main__":
     import os
